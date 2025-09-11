1d22d3b9e9e3205d2e659deb80ae0291ba85651f
KELLZ <forestruns@gmail.com>
2025-09-10 23:16:47 -0230

    rare cards are rarer

--- External/KZ/StockRun/StockRun.s
+++ External/KZ/StockRun/StockRun.s
@@ -115,7 +115,7 @@
 # StockRun Vars
   .set SR_STOCK_COUNT, 4
   .set SR_CRIT_CHANCE, 3 # 3% chance to crit
-  .set SR_RARE_CHANCE, 25
+  .set SR_RARE_CHANCE, 5
   .set SR_SHIELD_DMG_AMT, 15
   .set SR_CRIT_CHANCE_GLASS, 10
 
